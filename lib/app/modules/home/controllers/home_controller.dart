0b3f9d3d47273f8088505f84a520f71c2cf56f83
Prateek Jain <jprateek601@gmail.com>
2022-09-04 15:58:36 +0530

    UI of home screen

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,5 +1,29 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:movify_app/app/data/models/dto/repo_response.dart';
+import 'package:movify_app/app/data/models/response/movie_response.dart';
+import 'package:movify_app/app/data/repository/movie_repository.dart';
 
 class HomeController extends GetxController {
+  late final MovieResponse movieResponse;
+  bool isLoading = true;
+  bool isError = false;
 
+  @override
+  void onInit() {
+    super.onInit();
+    getPopularMovies();
+  }
+
+  Future<void> getPopularMovies() async {
+    RepoResponse<MovieResponse> res = await MovieRepository.getPopularMovies();
+    isLoading = false;
+    if(res.error == null) {
+      movieResponse = res.data!;
+    } else {
+      isError = true;
+      debugPrint(res.error?.message);
+    }
+    update();
+  }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
+import 'package:movify_app/widgets/movie_list_widget.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -10,15 +10,45 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 30),
+              child: Text(
+                'Popular movies',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                ),
+              ),
+            ),
+            GetBuilder<HomeController>(
+              builder: (_) => controller.isLoading
+                  ? const Loader()
+                  : controller.isError
+                      ? const Text('Error in fetching data')
+                      : MovieListWidget(controller.movieResponse.results!),
+            ),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class Loader extends StatelessWidget {
+  const Loader({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.only(top: 100),
+      child: Center(
+        child: CircularProgressIndicator(),
       ),
     );
   }
